0e754802b5f121e262c52b41fb5cc51eb7432d85
zhaowei <261626314@qq.com>
2019-12-20 00:05:17 +0800

    创建项目

--- src/main/java/com/zhaochenxu/cn/day03/demo01.java
+++ src/main/java/com/zhaochenxu/cn/day03/demo01.java
@@ -1,4 +1,25 @@
 package com.zhaochenxu.cn.day03;
 
-public class demo01 {
+
+
+public class demo01 extends Object {
+
+
+    public static void methodName(){
+
+        System.out.println("这是一个方法");
+        return;
+    }
+
+
+    public static void main(String[] args) {
+        methodName();
+        methodName1();
+
+
+    }
+
+    private static void methodName1() {
+        System.out.println("zhe shi dier ge fangfa");
+  }
 }
