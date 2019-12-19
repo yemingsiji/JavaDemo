611a296bd98584010d18923518a613414279fc91
zhaowei <261626314@qq.com>
2019-12-20 00:44:29 +0800

    创建项目

---
+++ src/main/java/com/zhaochenxu/cn/day03/demo2.java
@@ -0,0 +1,18 @@
+package com.zhaochenxu.cn.day03;
+
+public class demo2 {
+
+
+
+    public static void main(String[] args) {
+        showDemo();
+
+
+    }
+
+    private static void showDemo() {
+
+        int i =(1<=3? 200:300);
+        System.out.println(i);
+    }
+}
