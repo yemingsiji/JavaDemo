74bea839bd2f7a792a42c661954bd9cbb9533baf
zhaowei <261626314@qq.com>
2019-12-26 00:05:28 +0800

    创建项目

--- src/main/java/com/zhaochenxu/cn/day03/DoWhile.java
+++ src/main/java/com/zhaochenxu/cn/day03/DoWhile.java
@@ -1,4 +1,17 @@
 package com.zhaochenxu.cn.day03;
 
 public class DoWhile {
+
+
+    public static void main(String[] args) {
+
+        int i = 10;
+        do {
+            System.out.println("hellowolrd"+i);
+            i++;
+
+        }while(i<=100);
+    }
+
+
 }
