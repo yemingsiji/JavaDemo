5593768568571f76ee60180529c6913af4ce6f85
yemingsiji <zw_chinese@126.com>
2019-12-26 15:34:42 +0800

    上传 day04

---
+++ src/main/java/com/zhaochenxu/cn/day04/MethodDemo02.java
@@ -0,0 +1,31 @@
+package com.zhaochenxu.cn.day04;
+
+public class MethodDemo02 {
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public static void open(){
+        return;
+    }
+    public static void open(int x){
+        return;
+    }
+
+    static void open(int x ,int y){
+        return;
+    }
+
+    public static void open (double x, double y){
+        return;
+    }
+
+    public static void open (int x ,double y){
+        return;
+    }
+
+
+}
